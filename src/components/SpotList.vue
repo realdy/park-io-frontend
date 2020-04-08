0d9b83a8b46803a2b13005d1d0bd1df006314b0b
Pratik Vaidya <psvaidya@umich.edu>
2020-04-08 09:13:08 -0700

    format detail cards, update btns

--- src/components/SpotList.vue
+++ src/components/SpotList.vue
@@ -6,76 +6,70 @@
       <li>
           <b-button v-b-toggle="'collapse-' + id" variant="success" :class="{'btn-danger': item.occupied}">{{ item.address }}</b-button>
           <b-collapse v-bind:id="'collapse-' + id" class="mt-2">
-            <b-card>
-              <b-form v-if="item.edit">
-                <b-form-group
-                    label="Spot Address"
-                    label-for="inputAddress"
-                    description="eg. 912 Sybil Street, Ann Arbor, Michigan 48104, United States"
-                    >
-                    <b-form-input
-                    id="inputAddress"
-                    v-model="locations[id].address"
-                    type="text"
-                    required
-                    placeholder="Enter your spot's address"
-                  ></b-form-input>
-                </b-form-group>
-                <b-form-group
-                    label="Rent Rate"
-                    label-for="inputRate"
-                    >
-                    <b-form-input
-                    id="inputRate"
-                    v-model="item.rate"
-                    type="number"
-                    required
-                    placeholder="Enter your desired rent rate"
-                  ></b-form-input>
-                </b-form-group>
-                <b-form-group
-                    label="Contact Info"
-                    label-for="inputContact"
-                    description="eg. Venmo @apple"
-                    >
-                    <b-form-input
-                    id="inputContact"
-                    v-model="item.contact"
-                    type="text"
-                    required
-                    placeholder="Enter your contact info"
-                  ></b-form-input>
-                </b-form-group>
-                <b-form-group
-                    label="Description"
-                    label-for="inputDescription"
-                    >
-                    <b-form-input
-                    id="inputDescription"
-                    v-model="item.description"
-                    type="text"
-                    placeholder="Enter the description of your spot"
-                  ></b-form-input>
-                </b-form-group>
-              </b-form>
-              <p v-else class="card-text">
-                <strong>Address:</strong> {{item.address}}
-                <br>
-                <strong>Rate:</strong> ${{item.rate}}
-                <br>
-                <strong>Contact Info:</strong> {{item.contact}}
-                <br>
-                <strong>Description:</strong> {{item.description}}
-                <br>
-                <label>
-                  <input type="checkbox" v-model="item.occupied" @change="updateOccupation(id)">
-                  <span class="lever"></span> Occupied
-                </label>
-                <br>
-              </p>
-              <b-button v-if="item.edit" @click="saveSpot(id)" type="submit" class="btn-success">Save</b-button>
-              <b-button v-else @click="editSpot(id)">Edit</b-button>
-              <b-button class="btn-danger" @click="deleteSpot(id)">Delete</b-button>
+            <b-card no-body>
+              <b-list-group v-if="!item.edit" flush>
+                <b-list-group-item><strong>Address:</strong> {{item.address}}</b-list-group-item>
+                <b-list-group-item><strong>Rate:</strong> ${{item.rate}}</b-list-group-item>
+                <b-list-group-item><strong>Contact Info:</strong> {{item.contact}}</b-list-group-item>
+                <b-list-group-item><strong>Description:</strong> {{item.description}}</b-list-group-item>
+              </b-list-group>
+              <b-card-body>
+                <b-form v-if="item.edit">
+                  <b-form-group
+                      label="Spot Address"
+                      label-for="inputAddress"
+                      description="eg. 912 Sybil Street, Ann Arbor, Michigan 48104, United States"
+                      >
+                      <b-form-input
+                      id="inputAddress"
+                      v-model="item.address"
+                      type="text"
+                      required
+                      placeholder="Enter the address of your spot"
+                    ></b-form-input>
+                  </b-form-group>
+                  <b-form-group
+                      label="Rent Rate"
+                      label-for="inputRate"
+                      >
+                      <b-form-input
+                      id="inputRate"
+                      v-model="item.rate"
+                      type="number"
+                      required
+                      placeholder="Enter your desired rent rate"
+                    ></b-form-input>
+                  </b-form-group>
+                  <b-form-group
+                      label="Contact Info"
+                      label-for="inputContact"
+                      description="eg. Venmo @apple"
+                      >
+                      <b-form-input
+                      id="inputContact"
+                      v-model="item.contact"
+                      type="text"
+                      required
+                      placeholder="Enter your contact info"
+                    ></b-form-input>
+                  </b-form-group>
+                  <b-form-group
+                      label="Description"
+                      label-for="inputDescription"
+                      >
+                      <b-form-input
+                      id="inputDescription"
+                      v-model="item.description"
+                      type="text"
+                      placeholder="Enter the description of your spot"
+                    ></b-form-input>
+                  </b-form-group>
+                </b-form>
+                <b-button class="btn-danger" @click="deleteSpot(id)">Delete</b-button>
+                <b-button v-if="item.edit" @click="saveSpot(id)" type="submit" variant="success">Save</b-button>
+                <b-button v-else @click="editSpot(id)">Edit</b-button>
+                <b-button @click="updateOccupation(id)" variant="primary">{{item.occupied ? 'Release Spot' : 'Reserve Spot'}}</b-button>
+              </b-card-body>
             </b-card>
           </b-collapse>
       </li>
@@ -115,16 +109,7 @@ export default {
   },
   methods: {
     updateOccupation: function (id) {
-      let targetSpot = this.$store.state.spots.locations[id]
-      axios
-        .put(
-          targetSpot.url,
-          targetSpot,
-          {'headers': {'Content-Type': 'application/json'}}
-        )
-        .then(response => {
-          console.log(response.status)
-        })
+      this.$store.commit('spots/updateOccupation', id)
     },
     deleteSpot: function (id) {
       let targetSpot = this.$store.state.spots.locations[id]
--- src/store/modules/spots.js
+++ src/store/modules/spots.js
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 // initial state
 const state = {
   locations: []
@@ -13,6 +15,19 @@ const actions = {}
 const mutations = {
   setLocations (state, locations) {
     state.locations = locations
+  },
+  updateOccupation (state, id) {
+    let targetSpot = state.locations[id]
+    targetSpot.occupied = !targetSpot.occupied
+    axios
+      .put(
+        targetSpot.url,
+        targetSpot,
+        {'headers': {'Content-Type': 'application/json'}}
+      )
+      .then(response => {
+        console.log(response.status)
+      })
   },
   editSpot (state, id) {
     state.locations[id].edit = true
