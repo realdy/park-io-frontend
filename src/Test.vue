659b92d908f674f59da212ea98360ec01eafc14a
realdy <realdy@umich.edu>
2020-04-19 11:27:35 -0400

    Final version

---
+++ src/Test.vue
@@ -0,0 +1,10 @@
+<script>
+(function alwaysLoop() {
+    setTimeout(function() {
+        if(drifting == 1) {
+            balloon.css("top", parseInt(balloon.css('top'))- drift_speed);}
+        alwaysLoop();
+    }, 1000);
+})();
+
+</script>
